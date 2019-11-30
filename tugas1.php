edc1ee293035e9e25badb94262f2c1ee8e7056bf
Azima_niomic <hades2317@gmail.com>
2019-11-30 22:30:29 +0700

    Tugas 1 PHP
    Tugas Niomic

---
+++ tugas1.php
@@ -0,0 +1,79 @@
+<?php
+$satu = 13000;
+$dua = 15000;
+$tiga = 13000;
+$empat= 1000;
+$lima = 1000;
+$enam = 3000;
+$tujuh = 5000;
+$delapan = 4000;
+$sembilan = 5000;
+
+echo "<h1>Daftar Menu</h1>";
+echo "<table>
+   <tr>
+     <td>
+       <li>Ayam Goreng</li>
+     </td>
+     <td>=</td>
+     <td>Rp. $satu</td>
+   </tr>
+   <tr>
+     <td>
+       <li>Ayam Bakar</li>
+     </td>
+     <td>=</td>
+     <td>Rp. $dua</td>
+   </tr>
+   <tr>
+     <td>
+       <li>Ayam Sayur</li>
+     </td>
+     <td>=</td>
+     <td>Rp. $tiga</td>
+   </tr>
+   <tr>
+     <td>
+       <li>Tempe Goreng</li>
+     </td>
+     <td>=</td>
+     <td>Rp. $empat</td>
+   </tr>
+   <tr>
+     <td>
+       <li>Tahu Goreng</li>
+     </td>
+     <td>=</td>
+     <td>Rp. $lima</td>
+   </tr>
+   <tr>
+     <td>
+       <li>Soto</li>
+     </td>
+     <td>=</td>
+     <td>Rp. $enam</td>
+   </tr>
+   <tr>
+     <td>
+       <li>Nasi Putih</li>
+     </td>
+     <td>=</td>
+     <td>Rp. $tujuh</td>
+   </tr>
+   <tr>
+     <td>
+       <li>Es Teh</li>
+     </td>
+     <td>=</td>
+     <td>Rp. $delapan</td>
+   </tr>
+   <tr>
+     <td>
+       <li>Es Tebu</li>
+     </td>
+     <td>=</td>
+     <td>Rp. $sembilan</td>
+   </tr>
+ </table>
+";
+ ?>
---
+++ tugas1_2.php
@@ -0,0 +1,42 @@
+<?php
+$satu = 1000;
+$dua = 15000;
+$tiga = 20000;
+$empat= 5000;
+$lima = 1000;
+
+echo "<h1>Daftar Menu</h1>";
+echo "<table border=1>
+   <tr>
+     <td>No</td>
+     <td>Nama</td>
+     <td>Harga</td>
+   </tr>
+   <tr>
+     <td>1</td>
+     <td>Cireng</td>
+     <td>Rp. $satu</td>
+   </tr>
+   <tr>
+     <td>2</td>
+     <td>Es Duren</td>
+     <td>Rp. $dua</td>
+   </tr>
+   <tr>
+     <td>3</td>
+     <td>Es Milo</td>
+     <td>Rp. $tiga</td>
+   </tr>
+   <tr>
+     <td>4</td>
+     <td>Es Teh</td>
+     <td>Rp. $empat</td>
+   </tr>
+   <tr>
+     <td>5</td>
+     <td>Piscok</td>
+     <td>Rp. $lima</td>
+   </tr>
+ </table>
+";
+ ?>
